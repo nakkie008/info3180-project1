f5ea99b47950ed95b87b9cec1f9985879065ec93
annia <bowesna278@gmail.com>
2023-03-12 20:40:14 -0500

    Created flask form

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, IntegerField, TextAreaField, SelectField, FloatField
 from wtforms.validators import InputRequired
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class PropertyForm(FlaskForm):
-    title = StringField('Username', validators=[InputRequired()])
-    num_bedrooms = PasswordField('Password', validators=[InputRequired()])
-
-    photo = FileField("Photo", validators=[FileRequired(), FileAllowed(["jpg", "png"], "Images only")])
+    title = StringField('Title', validators=[InputRequired()])
+    num_bedrooms = IntegerField('Number of Bedrooms', validators=[InputRequired()])
+    num_bathrooms = IntegerField('Number of Bathrooms', validators=[InputRequired()])
+    location = StringField('Location', validators=[InputRequired()])
+    price = FloatField('Price', validators=[InputRequired()])
+    type = SelectField('Type', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired()])
+    photo = FileField('Photo', validators=[FileRequired(), FileAllowed(["jpg", "png"], "Images only")])
 
     
